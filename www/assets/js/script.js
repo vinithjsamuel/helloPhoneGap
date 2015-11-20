cf0f78eb51c40197378aa51e5dfda7d111fbae29
nimyhubald <nimmy@getguzzle.com>
2015-11-20 17:59:15 +0000

    fade

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -180,8 +180,8 @@ if(window.localStorage.getItem("outlets") != undefined )
 
   $rootScope.homemaintitle = true;
   setTimeout(function(){
-    $("#status").fadeOut(); $("#preloader").delay(450).fadeOut("slow");
-  }, 7500);
+    $("#status").fadeOut("slow"); $("#preloader").delay(350).fadeOut("slow");
+  }, 8000);
   $rootScope.storage = JSON.parse(window.localStorage['outlets' || '{}']);
 
   var datas=$rootScope.storage;
