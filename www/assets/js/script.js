a1db728cb2e928fa4a1792a486df1cb0c807d04f
nimyhubald <nimmy@getguzzle.com>
2015-11-26 22:10:59 +0400

    Update script.js

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -409,11 +409,6 @@ if(window.localStorage.getItem("outlets") != undefined )
             }
           }
 
-          var options = { timeout: 30000 };
-          var watchID = null;
-          
-          watchID = navigator.geolocation.getCurrentPosition($scope.showPositions, $scope.showErrors, options);
-
 
           $scope.filterMakes = function ()
           {
@@ -659,10 +654,6 @@ if(window.localStorage.getItem("outlets") != undefined )
           }
           $scope.getLocation();
           
-          var options = { timeout: 30000 };
-          var watchID = null;
-          
-          watchID = navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError, options);
 
         }).error(function(){
 
